bb979983b40b88b8607475c5cc57277bb13cb067
Abysemilla97 <abby.cymon@gmail.com>
2023-07-24 16:43:56 -0700

    Added new files to print strings and characters

---
+++ myputchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * my_putchar - a function that writes the character c to stdout.
+ * @c: the character to print.
+ *
+ * Return: on success 1, on error -1.
+ */
+int my_putchar(char c)
+{
+	return (write(1, &c, 1));
+}
---
+++ printspring.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_string - a function that prints a string.
+ * @str: the string to print.
+ *
+ * Return: the number of characters to be printed.
+ */
+int print_string(char *str)
+{
+	int num_of_characters_printed = 0;
+
+	for (; str[i] != '\0'; i++)
+	{
+		num_of_characters_printed += my_putchar(str[i]);
+	}
+
+	return (num_of_characters_printed);
+}
