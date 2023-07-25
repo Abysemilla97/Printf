98ea58f41709b47f841bbe6664bfae5d96d6b889
Abysemilla97 <abby.cymon@gmail.com>
2023-07-24 17:21:56 -0700

    A function that prints string of char

--- printspring.c
+++ printspring.c
@@ -2,20 +2,18 @@ include "main.h"
 
 /**
  * print_string - a function that prints a string.
- * @str: the string to print.
+ * @str: the string to be printed.
  *
  * Return: the number of characters to be printed.
  */
 int print_string(char *str)
 {
-        int num_of_characters_printed = 0;
+	int num_of_characters_printed = 0;
 
-        for (; str[i] != '\0'; i++)
-        {
-                num_of_characters_printed += my_putchar(str[i]);
-        }
+	for (; str[i] != '\0'; i++)
+	{
+		num_of_characters_printed += my_putchar(str[i]);
+	}
 
-        return (num_of_characters_printed);
+	return (num_of_characters_printed);
 }
-
-
