0de1de6bc1db002933a43b400a4bf8cbfc085df5
Abysemilla97 <abby.cymon@gmail.com>
2023-07-25 06:43:44 -0700

    a function that comverts decilal to binary.

---
+++ binary_spec.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * binary_specifier - that converts a decimal number into
+ * a binary number and stores it in an array called abby.
+ * @decimal_num: list of num in array.
+ *
+ * Return: numbers of char printed.
+ */
+int binary_specifier(int decimal_num)
+{
+	int abby[32];
+	int i = 0;
+
+	num_of_characters_printed = 0;
+
+	while (decimal_num >= 2) /* loop breaks when digit is < 2 */
+	{
+		abby[i] = decimal_num % 2;
+		decimal_num /= 2;
+		i++;
+	}
+
+	abby[i] = decimal_num;
+
+	for (i; i >= 0; i--)
+	{
+		num_of_characters_printed += my_putchar(abby[i] + '0');
+	}
+
+	return (num_of_characters_printed);
+}
+
