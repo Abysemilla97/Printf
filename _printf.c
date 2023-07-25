0a8249d17723b43370bbd8c7432daeac1a2a0f5d
Tednic <replyoku@gmail.com>
2023-07-25 16:19:05 +0100

    _printf updated 40lones of code Betty

--- _printf.c
+++ _printf.c
@@ -1,35 +1,24 @@
-/*
- * Task: Write a function that produces output according to a format
- * Prototype: int _printf(const char *format, ...)
- * format is a character string with >= 0 directives
- * format specifiers c, s, and % are considered in this task
- * Author: Abigail Simon & Oku Umoren
- */
-
 #include <stdarg.h>
 #include "main.h"
 #include <stdio.h>
 
 /**
- * _printf - a function that produces output according to a format
- * @format: a character string with zero or more directives
+ * _printf - function produces output using a format
+ * @format: a character string with directives
  * @...: varaible arguments passed to _printf function
- * Return: the number of characters printed without null bytes
+ * Return: the number of char printed without null bytes
  */
 
 int _printf(const char *format, ...)
 {
-	int num_of_characters_printed = 0; /* num of characters returned */
+	int num_of_characters_printed = 0;
 	int i = 0; /* tracks iteration in loop */
 	va_list checklist; /* Declaration of checklist */
 
-	va_start(checklist, format); /* checklist starts from format */
-
+	va_start(checklist, format);
 	if (format == NULL)
 		return (-1);
-
-	for (; format[i] != '\0';) /* '\0' marks end of string */
-		/* loops through string format until null character */
+	for (; format[i] != '\0';)
 	{
 		if (format[i] != '%')
 		{
